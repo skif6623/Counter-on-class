00a607b06f0ce0c6eef1c64d9b6d926519d035b8
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-27 16:53:26 +0200

    added event to component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,8 +7,12 @@ export class Сounter extends Component {
 			<div className="Counter">
 				<span className="Counter__value">0</span>
 				<div className="Counter__controls">
-					<button type="button">Збільшити на 1</button>
-					<button type="button">Зменшити на 1</button>
+					<button type="button" onClick={() => console.log("Збільшити")}>
+						Збільшити на 1
+					</button>
+					<button type="button" onClick={() => console.log("Зменшити")}>
+						Зменшити на 1
+					</button>
 				</div>
 			</div>
 		);
