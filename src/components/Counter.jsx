98ffef9fbb0009c4883d204521f41b0c15c9645e
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-27 17:27:27 +0200

    add inc and dec to public property

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,15 +2,24 @@ import React, {Component} from "react";
 import "./Counter.css";
 
 export class Сounter extends Component {
+	handleIncrement = () => {
+		console.log("Збільшили на 1");
+		console.log(this);
+	};
+
+	handleDecrement = () => {
+		console.log("Зменшили на 1");
+		console.log(this);
+	};
 	render() {
 		return (
 			<div className="Counter">
 				<span className="Counter__value">0</span>
 				<div className="Counter__controls">
-					<button type="button" onClick={() => console.log("Збільшити")}>
+					<button type="button" onClick={this.handleIncrement}>
 						Збільшити на 1
 					</button>
-					<button type="button" onClick={() => console.log("Зменшити")}>
+					<button type="button" onClick={this.handleDecrement}>
 						Зменшити на 1
 					</button>
 				</div>
