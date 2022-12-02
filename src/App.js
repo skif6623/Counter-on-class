7e96c7c33f40821de0babfa0475e6fb00cc16e53
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-02 21:18:21 +0200

    added check to todo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // import {Counter} from "./components/Counter/Counter";
 // import {Dropdown} from "./components/Dropdown/Dropdown";
 import React, { Component } from 'react';
-// import { TodoList } from './components/TodoList/TodoList';
+import { TodoList } from './components/TodoList/TodoList';
 import initTodos from './todos.json';
 // import { Form } from './components/Form/Form';
-import { ColorPicker } from './components/ColorPicker/ColorPicker';
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+// import { ColorPicker } from './components/ColorPicker/ColorPicker';
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 export class App extends Component {
   state = {
     todos: initTodos,
@@ -24,29 +24,46 @@ export class App extends Component {
     }));
   };
 
+  toggleCompleted = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        return todo.id === todoId
+          ? { ...todo, completed: !todo.completed }
+          : todo;
+      }),
+    }));
+  };
+
   formSubmitHandler = data => {
     console.log('Дані які приходять з форми', data);
   };
 
   render() {
-    // const {todos} = this.state;
+    const { todos } = this.state;
 
-    // const completeTodos = todos.reduce((total, todo) => (todo.completed ? total + 1 : total), 0);
+    const completeTodos = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0
+    );
 
     return (
       <>
-        <ColorPicker options={colorPickerOptions} />
+        {/* <ColorPicker options={colorPickerOptions} /> */}
         {/* <Form submit={this.formSubmitHandler} /> */}
-        {/* <h1>Стан компонента</h1>
-				<ul>
-					<li>
-						<p>Загальна к-сть: {todos.length}</p>
-					</li>
-					<li>
-						<p>Виконані: {completeTodos} </p>
-					</li>
-				</ul>
-				<TodoList todos={todos} onDeleteTodo={this.deleteTodo} /> */}
+        <h1>Стан компонента</h1>
+        <ul>
+          <li>
+            <p>Загальна к-сть: {todos.length}</p>
+          </li>
+          <li>
+            <p>Виконані: {completeTodos} </p>
+          </li>
+        </ul>
+        <TodoList
+          todos={todos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
       </>
     );
   }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,13 +1,18 @@
-import {TodoListItem} from "./TodoList.styled.js";
-export const TodoList = ({todos, onDeleteTodo}) => {
-	return (
-		<ul>
-			{todos.map(({id, text, completed}) => (
-				<TodoListItem key={id}>
-					<p>{text}</p>
-					<button onClick={() => onDeleteTodo(id)}>Видалити</button>
-				</TodoListItem>
-			))}
-		</ul>
-	);
+import { TodoListItem } from './TodoList.styled.js';
+export const TodoList = ({ todos, onDeleteTodo, onToggleCompleted }) => {
+  return (
+    <ul>
+      {todos.map(({ id, text, completed }) => (
+        <TodoListItem key={id}>
+          <input
+            type="checkbox"
+            checked={completed}
+            onChange={() => onToggleCompleted(id)}
+          />
+          <p>{text}</p>
+          <button onClick={() => onDeleteTodo(id)}>Видалити</button>
+        </TodoListItem>
+      ))}
+    </ul>
+  );
 };
