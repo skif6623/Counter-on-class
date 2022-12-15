9f15d7f71803aebcea9a4a0fbf2e7d3bc7d7ae0e
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-15 17:30:09 +0200

    useReducer with obj

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -4,10 +4,10 @@ import './Counter.css';
 function countReducer(state, actions) {
   switch (actions.type) {
     case 'increment':
-      return state + actions.payload;
+      return { ...state, count: state.count + actions.payload };
 
     case 'decrement':
-      return state - actions.payload;
+      return { ...state, count: state.count - actions.payload };
 
     default:
       return state;
@@ -15,11 +15,11 @@ function countReducer(state, actions) {
 }
 
 export function Counter() {
-  const [count, dispatch] = useReducer(countReducer, 0);
+  const [state, dispatch] = useReducer(countReducer, { count: 0 });
 
   return (
     <div className="Counter">
-      <p>{count}</p>
+      <p>{state.count}</p>
       <button
         type="button"
         onClick={() => dispatch({ type: 'decrement', payload: 1 })}
